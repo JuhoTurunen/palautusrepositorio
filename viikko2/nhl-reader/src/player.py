ec3012cdb7f25b13d9131869f99c30ea1660cd7c
Juho Turunen <juho.turunen89@gmail.com>
2024-11-11 23:40:56 +0200

    Improved printing of results

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -6,4 +6,8 @@ class Player:
         self.assists = data.get("assists")
 
     def __str__(self):
-        return f"{self.name} team {self.team}  goals {self.goals} assists {self.assists}"
+        a = (f"{self.name}{" " * max(28-len(self.name), 1)}{self.team}   " 
+        f"{self.goals}{" " * max(3-len(str(self.goals)), 1)}+ "
+        f"{self.assists}{" " * max(3-len(str(self.assists)), 1)}= "
+        f"{self.goals + self.assists}")
+        return a
