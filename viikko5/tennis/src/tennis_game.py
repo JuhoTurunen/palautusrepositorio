bca8ee2ea2b3f260c436ed7bb205a570cf54dc37
Juho Turunen <juho.turunen89@gmail.com>
2024-12-02 22:56:43 +0200

    Refactored tennis_game

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,51 @@
 class TennisGame:
+    DEUCE_THRESHOLD = 3
+    WIN_THRESHOLD = 4
+
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self.player1_score == self.player2_score:
+            return self.tied_to_game_state_str()
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        if self.player1_score < self.WIN_THRESHOLD and self.player2_score < self.WIN_THRESHOLD:
+            score1 = self.number_to_tennis_score(self.player1_score)
+            score2 = self.number_to_tennis_score(self.player2_score)
+            return f"{score1}-{score2}"
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+        return self.lead_to_game_state_str()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def tied_to_game_state_str(self):
+        if self.player1_score >= self.DEUCE_THRESHOLD:
+            return "Deuce"
+        tennis_score = self.number_to_tennis_score(self.player1_score)
+        return f"{tennis_score}-All"
 
-        return score
+    def lead_to_game_state_str(self):
+        game_balance = self.player1_score - self.player2_score
+        leading_player = self.player1_name if game_balance > 0 else self.player2_name
+        lead = abs(game_balance)
+        if lead == 1:
+            return f"Advantage {leading_player}"
+        return f"Win for {leading_player}"
+
+    def number_to_tennis_score(self, number):
+        match number:
+            case 0:
+                return "Love"
+            case 1:
+                return "Fifteen"
+            case 2:
+                return "Thirty"
+            case 3:
+                return "Forty"
